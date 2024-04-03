7ae7083bb4108846704f1e3cf7b5217eb67d315f
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-03 11:59:33 -0300

    init: spring-boot-project with spring-web, spring-dev-tolls and lombok

---
+++ src/main/java/com/marcosparreiras/nlw_14/Nlw14Application.java
@@ -0,0 +1,13 @@
+package com.marcosparreiras.nlw_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nlw14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nlw14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosparreiras/nlw_14/Nlw14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosparreiras.nlw_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nlw14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
