79a7df7cedbd2ab12f52902c16c71cd926198993
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-08 17:49:47 -0300

    feat: add jpa annotation on studenets entities

--- src/main/java/com/marcosparreiras/nlw_14/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/marcosparreiras/nlw_14/modules/students/entities/CertificationStudentEntity.java
@@ -1,19 +1,47 @@
 package com.marcosparreiras.nlw_14.modules.students.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
-  private UUID studentId;
+
+  @Column(nullable = false)
   private String technology;
+
+  @Column(nullable = false)
   private int great;
+
+  @Column(name = "student_id")
+  private UUID studentId;
+
+  @ManyToOne
+  @JoinColumn(name = "student_id", insertable = false, updatable = false)
+  private StudentEntity studentEntity;
+
+  @OneToMany(mappedBy = "certificationStudentEntity")
   private List<AnswersCertificationEntity> answersCertificationEntities;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
