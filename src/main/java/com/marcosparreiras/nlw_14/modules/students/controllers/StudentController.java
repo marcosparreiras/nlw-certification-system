43fdbe32df42fdaa84cb2e852942ad729763b05e
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-08 17:05:51 -0300

    config: add docker compose to run postgress and add jpa and postgresql dependencies

---
+++ src/main/java/com/marcosparreiras/nlw_14/modules/students/controllers/StudentController.java
@@ -0,0 +1,8 @@
+package com.marcosparreiras.nlw_14.modules.students.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {}
